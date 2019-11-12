e864023993b5ab939817a10e2b99ce2379ca7f5b
Matthes <matthes@leuffen.de>
2019-11-12 23:10:18 +0100

    added node-expressjs

---
+++ node-expressjs/server.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+
+});
+
+
+app.listen(4100, function () {
+  console.log('Example app listening on port 4100!');
+});
