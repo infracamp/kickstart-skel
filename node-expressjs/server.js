49379d1dc70a408fcc206f61f32c81b93afd2ef0
Matthes <matthes@leuffen.de>
2019-11-13 01:22:01 +0100

    update node setup

--- node-expressjs/server.js
+++ node-expressjs/server.js
@@ -1,14 +1,14 @@
 
-var express = require('express');
+import express from 'express';
+import {VERSION_INFO} from "./_config";
+
 var app = express();
 
-
 app.get('/', function (req, res) {
-  res.json({msg: 'Hello World!'});
+  res.json({success: true, msg: 'Hello World!'});
 
 });
 
-
-app.listen(4100, function () {
-  console.log('Example app listening on port 4100!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 4100! ' + VERSION_INFO);
 });
